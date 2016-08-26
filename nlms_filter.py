8302de53d5bba92526b7e43ad0ba2d0f19d615b5
Ian <wzhan366@uwo.ca>
2016-08-25 21:42:26 -0400

    Add files via upload
    Normalised Least Mean Squares Filter, comparing with lsm filter this one changes the weight matrix.

---
+++ nlms_filter.py
@@ -0,0 +1,22 @@
+"""Normalised Least Mean Squares Filter"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+
+def nlms_filter(signal, order = 4, mu = 1 ):
+    weight = np.zeros(order) # initial weight, the system model
+    length = len(signal) #find train signal length
+    estimate = [] # LSM filter output
+    er = [] # error list
+    for i in range(length-order):
+        x_signal = list(signal[i:i+order])# slice input signal
+        x = x_signal
+        x_signal.reverse() #transform sliced signal to timeline
+        x_signal = np.transpose(x_signal) # x_signal transpose
+        y_current = signal[i] #current signal point
+        y_estimate = np.dot(weight, x_signal) #use LSM estamate current signal
+        estimate.append(y_estimate) # filter output
+        error = y_current - y_estimate
+        er.append(error)
+        weight = weight + (mu * error * x_signal)/(x_signal* x)
+    return estimate, weight,er
